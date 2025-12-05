a4a853e74a12167a20b1d4023bd570fd9b7da52c
williamcdavies <152814048+williamcdavies@users.noreply.github.com>
2025-12-05 11:14:43 -0800

    Initial commit

---
+++ smoke_lake_poly/q.sql
@@ -0,0 +1,74 @@
+CREATE TEMP TABLE xref AS (
+    SELECT
+        l."Hylak_id"
+            AS l_id,
+        s.id
+            AS s_id,
+        s."Year"
+            AS year,
+        SUBSTRING(s."Start" FROM 1 FOR 7)
+            AS start,
+        SUBSTRING(s."End" FROM 1 FOR 7)
+            AS end,
+        s."Density"
+            AS density
+    FROM public.lakes_polys l
+    JOIN public.hms_smokes{{YEAR}} s
+        ON ST_Intersects(
+            l."4326_geometry",
+            s.geometry
+        )
+);
+
+CREATE TEMP TABLE xref_expanded AS (
+    SELECT
+        l_id,
+        s_id,
+        year,
+        (TO_DATE(start, 'YYYYDDD') + i)::date 
+            AS day,
+        density
+    FROM xref,
+    generate_series(
+        0,
+        (TO_DATE("end", 'YYYYDDD') - TO_DATE(start, 'YYYYDDD'))::int
+    ) AS i
+);
+
+CREATE TEMP TABLE xref_ranked AS (
+    SELECT
+        l_id,
+        year,
+        day,
+        MAX(
+            CASE COALESCE(density, '')
+                WHEN 'Heavy'  THEN 3
+                WHEN 'Medium' THEN 2
+                WHEN 'Light'  THEN 1
+                ELSE 0
+            END
+        )
+            AS density_rank
+    FROM xref_expanded
+    GROUP BY l_id, year, day
+);
+
+CREATE TEMP TABLE out AS (
+    SELECT
+        day
+            AS date,
+        COUNT(CASE WHEN density_rank = 1 THEN 1 END) 
+            AS l,
+        COUNT(CASE WHEN density_rank = 2 THEN 1 END) 
+            AS m,
+        COUNT(CASE WHEN density_rank = 3 THEN 1 END) 
+            AS h,
+        COUNT(CASE WHEN density_rank = 0 THEN 1 END) 
+            AS na,
+        COUNT(*) AS sum
+    FROM xref_ranked
+    GROUP BY date
+    ORDER BY date
+);
+
+COPY out TO STDOUT WITH CSV HEADER;
